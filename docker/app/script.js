eef5ee5338b963341103e161d92b65c4c0544984
sudheerpalepu <146517155+sudheerpalepu@users.noreply.github.com>
2026-02-28 22:10:27 +0000

    update 1

--- docker/app/script.js
+++ docker/app/script.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").addEventListener("click", function() {
     const greeting = document.getElementById("greeting");
-    greeting.textContent = "Hello Docker! ";
+    greeting.textContent = "Hello Docker sudheer welcome! ";
     alert("You clicked the button!");
 });
